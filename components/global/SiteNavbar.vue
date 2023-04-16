49bc63d4c30a4ad171aa3a633f962ee184c84322
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-16 19:58:02 +0100

    feat: create site navbar component
    Create site navbar component

---
+++ components/global/SiteNavbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <b-navbar>
+    <b-navbar-brand>
+      {{ $t("APP_NAME") }}
+    </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item :to="{ name: 'index' }">{{ $t("NAV__HOME") }}</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item :to="{ name: 'auth-login' }">{{ $t("NAV__LOGIN") }}</b-nav-item>
+      <b-nav-item :to="{ name: 'auth-signup' }">{{ $t("NAV__SIGNUP") }}</b-nav-item>
+      <b-nav-dropdown v-if="showLanguageSwitcher" id="nav--lang-dropdown" toggle-class="nav-link-custom" right>
+        <template slot="text">
+          {{ $t("NAV__LANG") }}
+        </template>
+        <b-dropdown-item
+          v-for="locale in availableLocales"
+          :key="locale.code + '-lang-dropdown-item'"
+          :to="switchLocalePath(locale.code)"
+          @click.prevent.stop="$event => $i18n.setLocale(locale.code)"
+        >
+          {{ generateLocaleLabel(locale.iso) }}
+        </b-dropdown-item>
+      </b-nav-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    name: "SiteNavbar",
+    computed: {
+      /**
+       * TODO Remove the static boolean from the filter lambda in this computed property
+       *
+       * Get's the available locales, currently modified to show all locales, including the active locale
+       */
+      availableLocales() {
+        return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale || true);
+      },
+      showLanguageSwitcher() {
+        const { availableLocales } = this;
+
+        return availableLocales > 1;
+      }
+    },
+    methods: {
+      /**
+       * Generates a display label based off the ISO code for a countries language.
+       *
+       * The way it does so is by either:
+       *
+       * If the ISO code DOES NOT contain a hyphen, the iso code is simply returned.
+       *
+       * If the ISO code DOES contain a hyphen, then the iso code is split at the hyphen, with the second part appended to the first, wrapped in parenthesis and upper-cased.
+       *
+       * @param {String} iso The ISO code of the language to generate a display label for.
+       *
+       * @returns {String} A display label for a countries language based off it's ISO code.  Ideal for use in language switchers and associated functionality.
+       */
+      generateLocaleLabel: iso => {
+        // Early return if ISO doesn't contain a hyphen
+        if (!iso.includes("-")) return iso;
+
+        // Else split at the hyphen
+        const isoParts = iso.split("-");
+
+        // Uppercase the second array value and wrap it in parenthesis using string interpolation
+        return `${isoParts[0]} (${isoParts[1].toUpperCase()})`;
+      }
+    }
+  };
+</script>
