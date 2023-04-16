171ee9de7b78ec0515aa40214a2c7cfd00d19a8c
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-16 19:56:22 +0100

    feat: create nuxt layouts
    Create generic default & blank Nuxt layout files

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <site-navbar />
+    <Nuxt style="min-height: calc(100vh - 56px)" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DefaultLayout",
+    components: {
+      SiteNavbar: () => import("~/components/global/SiteNavbar")
+    },
+    computed: {
+      /**
+       * TODO: Remove the static boolean from the filter lambda in this computed property
+       *
+       * Get's the available locales, currently modified to show all locales, including the active locale
+       */
+      availableLocales() {
+        return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale || true);
+      }
+    },
+    methods: {
+      /**
+       * Generates a display label based off the ISO code for a countries language.
+       *
+       * The way it does so is by either:
+       *
+       * If the ISO code DOES NOT contain a hyphen, the iso code is simply returned.
+       *
+       * If the ISO code DOES contain a hyphen, then the iso code is split at the hyphen, with the second part appended to the first, wrapped in parenthesis and upper-cased.
+       *
+       * @param {String} iso The ISO code of the language to generate a display label for.
+       *
+       * @returns {String} A display label for a countries language based off it's ISO code.  Ideal for use in language switchers and associated functionality.
+       */
+      generateLocaleLabel: iso => {
+        // Early return if ISO doesn't contain a hyphen
+        if (!iso.includes("-")) return iso;
+
+        // Else split at the hyphen
+        const isoParts = iso.split("-");
+
+        // Uppercase the second array value and wrap it in parenthesis using string interpolation
+        return `${isoParts[0]} (${isoParts[1].toUpperCase()})`;
+      }
+    }
+  };
+</script>
