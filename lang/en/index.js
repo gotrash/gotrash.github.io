676ada302ea71132315b7ba9a11d9171b2e06774
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-16 19:56:47 +0100

    feat: update locale file structure
    Update the structure of the language data within the app, so it's
    compatible with Nuxt and no longer a child of the `src` folder

---
+++ lang/en/index.js
@@ -0,0 +1,26 @@
+export default {
+  // General
+  APP_NAME: "GoTrash",
+  APP_NAME_LONG: "GoTrash UK",
+  // Nav Links
+  NAV__HOME: "Home",
+  NAV__LANG: "Lang",
+  NAV__LOGIN: "Login",
+  NAV__SIGNUP: "Signup",
+  // Page Titles
+  PAGE_TITLE__HOME: "Welcome to GoTrash",
+  PAGE_TITLE__LOGIN: "Login",
+  PAGE_TITLE__SIGNUP: "Signup",
+  message: "hello i18n !!",
+  // Auth
+  "AUTH.ERROR.USERNAME": "Username is required and must be 4 or more characters",
+  "AUTH.ERROR.PASSWORD": "Password is required and must be at least 8 characters",
+  "AUTH.LABEL.EMAIL": "Email",
+  "AUTH.LABEL.USERNAME": "Username",
+  "AUTH.LABEL.PASSWORD": "Password",
+  "AUTH.LABEL.CONFIRM_PASSWORD": "Confirm Password",
+  // General
+  "GENERAL.LABEL.CANCEL": "Cancel",
+  "GENERAL.LABEL.RESET": "Reset",
+  "GENERAL.LABEL.SUBMIT": "Submit"
+};
