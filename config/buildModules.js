03d88a72be6e6bce815c7a887a66a18cd01a0a4f
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-17 04:12:15 +0100

    fix: turn off Stylelint
    Temporarily turn off Stylelint

--- config/buildModules.js
+++ config/buildModules.js
@@ -1,6 +1,11 @@
 export default [
   // https://go.nuxtjs.dev/eslint
-  "@nuxtjs/eslint-module",
+  "@nuxtjs/eslint-module"
   // https://go.nuxtjs.dev/stylelint
-  "@nuxtjs/stylelint-module"
+  // [
+  //   "@nuxtjs/stylelint-module",
+  //   {
+  //     rules: { "selector-anb-no-unmatchable": null }
+  //   }
+  // ]
 ];
--- stylelint.config.js
+++ stylelint.config.js
@@ -3,5 +3,7 @@ module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-recommended-vue", "stylelint-config-prettier"],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  rules: {
+    "selector-anb-no-unmatchable": null
+  }
 };
