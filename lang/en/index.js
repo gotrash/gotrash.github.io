db0009f3595ad0f97712a1879fd71ff9d39d3925
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-18 21:39:44 +0100

    feat: translated strings
    Commit translated strings

--- lang/en/index.js
+++ lang/en/index.js
@@ -11,16 +11,37 @@ export default {
   PAGE_TITLE__HOME: "Welcome to GoTrash",
   PAGE_TITLE__LOGIN: "Login",
   PAGE_TITLE__SIGNUP: "Signup",
+  PAGE_TITLE__FORGOT_PASSWORD: "Forgot Password",
   message: "hello i18n !!",
   // Auth
-  "AUTH.ERROR.USERNAME": "Username is required and must be 4 or more characters",
   "AUTH.ERROR.PASSWORD": "Password is required and must be at least 8 characters",
-  "AUTH.LABEL.EMAIL": "Email",
-  "AUTH.LABEL.USERNAME": "Username",
-  "AUTH.LABEL.PASSWORD": "Password",
+  "AUTH.ERROR.USERNAME": "Username is required and must be 4 or more characters",
   "AUTH.LABEL.CONFIRM_PASSWORD": "Confirm Password",
+  "AUTH.LABEL.EMAIL": "Email",
+  "AUTH.LABEL.FORGOT_PASSWORD": "Forgot Password",
+  "AUTH.LABEL.LOGIN": "Login",
+  "AUTH.LABEL.PASSWORD": "Password",
+  "AUTH.LABEL.REMEMBER_ME": "Remember Me",
+  "AUTH.LABEL.REQUEST_NEW_PASSWORD": "Request New Password",
+  "AUTH.LABEL.SIGN_IN": "Sign In",
+  "AUTH.LABEL.SIGN_UP": "Sign Up",
+  "AUTH.LABEL.USERNAME": "Username",
+  "AUTH.MESSAGE.CONFIRM_FORGOT_PASSWORD": `
+    <p class="lead">
+      If we've found an account associated with the email address provided, then you will receive a password reset link
+      in your inbox shortly.
+    </p>
+
+    <p>
+      Sometimes the link can go to your spam/junk folder(s). If you're having issues finding the link
+      and you're sure you entered the correct email associated with your account, please check these folder(s).
+    </p>
+  `,
+  "AUTH.MESSAGE.FORGOT_PASSWORD": "You forgot your password? Here you can easily retrieve a new password.",
+  "AUTH.MESSAGE.SIGN_IN": "Sign in to start your session",
   // General
   "GENERAL.LABEL.CANCEL": "Cancel",
   "GENERAL.LABEL.RESET": "Reset",
-  "GENERAL.LABEL.SUBMIT": "Submit"
+  "GENERAL.LABEL.SUBMIT": "Submit",
+  "GENERAL.MESSAGE.THANK_YOU": "Thank You!!"
 };
