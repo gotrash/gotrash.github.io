2562139963fcd79820f175e59fcb07f709a26c61
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-18 21:38:33 +0100

    feat: create auth card header component
    Create authentication page header component

---
+++ components/global/AuthCardHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-card-header header-class="text-center">
+    <b-link class="h1" :to="{ name: 'index' }"><b>Go</b>Trash</b-link>
+  </b-card-header>
+</template>
+<script>
+  export default {
+    name: "AuthCardHeader"
+  };
+</script>
