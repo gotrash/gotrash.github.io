13fee8d755051c8f861a805b04ca84eee8a7e12f
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-18 21:39:08 +0100

    feat: create auth layout
    Create the shared authentication page layout

---
+++ layouts/auth.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="auth-page-layout">
+    <Nuxt />
+  </div>
+</template>
+<script>
+  export default {
+    name: "AuthLayout",
+    components: {
+      SiteNavbar: () => import("~/components/global/SiteNavbar.vue")
+    }
+  };
+</script>
+<style lang="scss">
+  #auth-page-layout {
+    -ms-flex-align: center;
+    align-items: center;
+    background-color: #e9ecef;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    height: 100vh;
+    -ms-flex-pack: center;
+    justify-content: center;
+  }
+</style>
