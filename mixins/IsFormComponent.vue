1a58e37ba6deefaa09c695f00c916b6e30a88ccf
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-20 15:55:05 +0100

    feat: create `IsFormComponent` mixin
    Create a mixin containing all common form functionality.
    
    Any generic handlers are in here and can be over-ridden by the implementing component.

---
+++ mixins/IsFormComponent.vue
@@ -0,0 +1,47 @@
+<script>
+  import { validationMixin } from "vuelidate";
+
+  export default {
+    mixins: [validationMixin],
+    computed: {
+      form: {
+        get() {
+          return this.value;
+        },
+        set(form) {
+          this.$emit("input", form);
+        }
+      }
+    },
+    mounted() {
+      console.log("Form Mounted: %s", this.$options.name);
+      console.log("Options: %o", this);
+    },
+    methods: {
+      onCancel() {
+        this.$emit("cancel");
+      },
+      onReset() {
+        this.$nextTick(() => {
+          this.$v.$reset();
+        });
+
+        this.$emit("reset");
+      },
+      onSubmit() {
+        this.$v.form.$touch();
+
+        if (this.$v.form.$anyError) {
+          return;
+        }
+
+        this.$emit("submit");
+      },
+      validateState(name) {
+        const { $dirty, $error } = this.$v.form[name];
+
+        return $dirty ? !$error : null;
+      }
+    }
+  };
+</script>
