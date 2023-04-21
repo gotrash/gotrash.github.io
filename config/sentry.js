1eb2fdabf7afd6cd1b2f1795c73b65ab64f98ccf
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-21 16:26:26 +0100

    fix: turn off Sentry debugging

--- config/sentry.js
+++ config/sentry.js
@@ -2,9 +2,7 @@ export default {
   dsn:
     process.env.SENTRY_DSN ||
     "https://4d5ae3c08af040e485d1a6d0115b42ac@o4505031174258688.ingest.sentry.io/4505031179108352",
-  config: {
-    debug: true
-  },
+  config: {},
   // initialize: true,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
