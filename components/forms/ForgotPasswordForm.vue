0ad6feef367ac317c1e148126b3fa03527c5f680
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-21 18:55:31 +0100

    Improve `ForgotPasswordForm` component

--- components/forms/ForgotPasswordForm.vue
+++ components/forms/ForgotPasswordForm.vue
@@ -1,21 +1,29 @@
 <template>
   <b-form @submit.prevent="onSubmit">
-    <b-input-group class="mb-3">
-      <template #append>
-        <b-input-group-text>
-          <fa-icon icon="envelope" />
-        </b-input-group-text>
-      </template>
+    <b-form-group>
+      <b-input-group>
+        <template #append>
+          <b-input-group-text>
+            <fa-icon icon="envelope" />
+          </b-input-group-text>
+        </template>
 
-      <b-form-input
-        id="forgot-password-email-input"
-        ref="email-input"
-        v-model="form.email"
-        name="email"
-        type="email"
-        :placeholder="$t('AUTH.LABEL.EMAIL')"
-      />
-    </b-input-group>
+        <b-form-input
+          id="forgot-password-email-input"
+          ref="email-input"
+          v-model="form.email"
+          name="email"
+          type="text"
+          :placeholder="$t('AUTH.LABEL.EMAIL')"
+          :state="validateState('email')"
+        />
+      </b-input-group>
+
+      <b-form-invalid-feedback :state="validateState('email')">
+        <template v-if="!$v.form.email.required">{{ $t("AUTH.ERROR.FORGOT_PASSWORD_EMAIL_REQUIRED") }}</template>
+        <template v-else-if="!$v.form.email.email">{{ $t("AUTH.ERROR.FORGOT_PASSWORD_EMAIL_INVALID") }}</template>
+      </b-form-invalid-feedback>
+    </b-form-group>
 
     <b-form-row>
       <b-col>
@@ -32,12 +40,13 @@
 </template>
 
 <script>
-  import { required } from "vuelidate/lib/validators";
+  import { validationMixin } from "vuelidate";
+  import { Validations } from "~/forms/ForgotPasswordForm";
   import IsFormComponent from "~/mixins/IsFormComponent";
 
   export default {
     name: "ForgotPasswordForm",
-    mixins: [IsFormComponent],
+    mixins: [IsFormComponent, validationMixin],
     props: {
       value: {
         type: Object,
@@ -45,12 +54,6 @@
         validator: obj => Object.prototype.hasOwnProperty.call(obj, "email")
       }
     },
-    validations: {
-      form: {
-        email: {
-          required
-        }
-      }
-    }
+    validations: Validations
   };
 </script>
