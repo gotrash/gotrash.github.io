ae21d6f853ea55547490c28eacaf7a43553e0fbf
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-04-21 15:43:51 +0100

    feat: create `HasFormData` mixin
    Create `HasFormData` mixin

---
+++ mixins/HasFormData.vue
@@ -0,0 +1,16 @@
+<script>
+  import HasDto from "~/mixins/HasDto";
+
+  export default {
+    mixins: [HasDto],
+    data() {
+      const { Dto } = this.$options;
+
+      return {
+        error: null,
+        errors: [],
+        form: new Dto()
+      };
+    }
+  };
+</script>
--- mixins/index.js
+++ mixins/index.js
@@ -1,2 +1,3 @@
 export { default as HasDto } from "./HasDto";
+export { default as HasFormData } from "./HasFormData";
 export { default as IsFormComponent } from "./IsFormComponent";
