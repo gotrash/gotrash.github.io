65ab5a36a1c28ccd0866c7bdd871442b7d2a4f67
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-19 21:23:02 +0100

    feat: create message spinner component
    Create message spinner component

---
+++ components/global/MessageSpinner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text-center text-primary">
+    <fa-icon :icon="icon" :class="[{ 'fa-spin': spinIcon }, 'fa-fw fa-3x mb-4']" />
+    <component :is="messageTag">{{ message }}...</component>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      icon: {
+        type: [Array, String],
+        required: false,
+        default() {
+          return "spinner";
+        }
+      },
+      message: {
+        type: String,
+        required: false,
+        default() {
+          return this.$t("GENERAL.MESSAGE.LOADING");
+        }
+      },
+      messageTag: {
+        type: String,
+        required: false,
+        default() {
+          return "h1";
+        }
+      },
+      showEllipsis: Boolean,
+      spinIcon: {
+        type: Boolean,
+        required: false,
+        default() {
+          return true;
+        }
+      }
+    }
+  };
+</script>
