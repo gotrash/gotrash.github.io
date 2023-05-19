add1a5ed3016c5a1d5cb84dd201621034ae4e0d6
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-19 21:23:28 +0100

    feat: create admin footer component
    * Create `AdminFooter` component

---
+++ components/footers/AdminFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="http://gotrash.co.uk">GoTrash</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block"><b></b></div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: "AdminFooter"
+  };
+</script>
