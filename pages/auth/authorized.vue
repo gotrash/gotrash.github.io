8467dcb9f0e160d96ffff9c654e60377a20eb877
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-20 22:35:26 +0100

    feat: create auth callback page
    Create authentication callback page

---
+++ pages/auth/authorized.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex align-items-center justify-content-around" style="height: 100vh">
+    <message-spinner :message="$t('AUTH.MESSAGE.AUTHORIZING')" show-ellipsis />
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      MessageSpinner: () => import("~/components/global/MessageSpinner.vue")
+    },
+    layout: "blank"
+    // auth: "guest"
+  };
+</script>
