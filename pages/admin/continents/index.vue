584bf4be0d6a124f2ebef6355aa24c2a903215a7
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-20 22:34:07 +0100

    feat: create continents page
    Create continents page

---
+++ pages/admin/continents/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="12">
+        <b-card body-bg-variant="white" footer-bg-variant="white" header-bg-variant="white" no-body>
+          <b-card-body>
+            <continents-table />
+          </b-card-body>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    components: {
+      ContinentsTable: () => import("~/components/tables/ContinentsTable.vue"),
+      MessageSpinner: () => import("~/components/global/MessageSpinner.vue")
+    },
+    layout: "admin",
+    data() {
+      return {
+        busy: true
+      };
+    }
+  };
+</script>
