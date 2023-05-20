54ede2a74161ccdebc53af4c8f71e2b8b6b3b555
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-20 22:27:34 +0100

    feat: update admin footer to set layout data
    Update the admin template footer to set layout data in VueX

--- components/footers/AdminFooter.vue
+++ components/footers/AdminFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
+  <footer ref="footer" class="main-footer">
     <strong>Copyright &copy; 2023 <a href="http://gotrash.co.uk">GoTrash</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block"><b></b></div>
@@ -9,6 +9,49 @@
 
 <script>
   export default {
-    name: "AdminFooter"
+    name: "AdminFooter",
+    computed: {
+      footerHeight: {
+        get() {
+          return this.$store.getters("layout/getHeight")("footer");
+        },
+        set(height) {
+          this.$store.commit("layout/setHeight", { key: "footer", height });
+        }
+      },
+      footerWidth: {
+        get() {
+          return this.$store.getters("layout/getWidth")("footer");
+        },
+        set(width) {
+          this.$store.commit("layout/setWidth", { key: "footer", width });
+        }
+      }
+    },
+    mounted() {
+      this.setHeight();
+      this.setWidth();
+
+      this.setupListeners();
+    },
+    unmounted() {
+      this.removeListeners();
+    },
+    methods: {
+      removeListeners() {
+        window.removeEventListener("resize", this.setHeight);
+        window.removeEventListener("resize", this.setWidth);
+      },
+      setHeight() {
+        this.footerHeight = this.$refs.footer.clientHeight + 1;
+      },
+      setWidth() {
+        this.footerWidth = this.$refs.footer.clientWidth;
+      },
+      setupListeners() {
+        window.addEventListener("resize", this.setHeight);
+        window.addEventListener("resize", this.setWidth);
+      }
+    }
   };
 </script>
