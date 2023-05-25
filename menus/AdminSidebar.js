239a4f625ea6ceb6bc147c7fdae3afcee6c4272a
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 12:28:24 +0100

    feat: add admin sidebar menu config file
    Add menu config for admin sidebar

---
+++ menus/AdminSidebar.js
@@ -0,0 +1,35 @@
+export default [
+  { icon: "gavel", route: "admin-bids", text: "NAV__BIDS" },
+  { icon: "globe", route: "admin-continents", text: "NAV__CONTINENTS" },
+  { icon: "globe-europe", route: "admin-countries", text: "NAV__COUNTRIES" },
+
+  // {
+  //   icon: "map-marker",
+  //   isDropdown: true,
+  //   route: "postcodes",
+  //   text: "NAV__POSTCODES",
+  //   items: [
+  //     { icon: "circle", route: "admin-postcodes-areas", text: "NAV__POSTCODE_AREAS" },
+  //     { icon: "circle", route: "admin-postcodes-units", text: "NAV__POSTCODE_UNITS" },
+  //     { icon: "circle", route: "admin-postcodes-sectors", text: "NAV__POSTCODE_SECTORS" },
+  //     { icon: "circle", route: "admin-postcodes-districts", text: "NAV__POSTCODE_DISTRICTS" }
+  //   ]
+  // },
+  { icon: "photo-video", route: "admin-files", text: "NAV__FILES" },
+  { icon: "trash", route: "admin-jobs", text: "NAV__JOBS" },
+  { icon: "trash", route: "admin-job-items", text: "NAV__JOB_ITEMS" },
+  { icon: "trash-alt", route: "admin-job-types", text: "NAV__JOB_TYPES" },
+  { icon: "unlock", route: "admin-password-reset-tokens", text: "NAV__PASSWORD_RESET_TOKENS" },
+  { icon: "lock", route: "admin-privileges", text: "NAV__PRIVILEGES" },
+  { icon: "lock", route: "admin-privilege-types", text: "NAV__PRIVILEGE_TYPES" },
+  { icon: "users", route: "admin-roles", text: "NAV__ROLES" },
+  { icon: "users", route: "admin-users", text: "NAV__USERS" },
+  { icon: "map-marker", route: "admin-user-locations", text: "NAV__USER_LOCATIONS" },
+  { icon: "user-check", route: "admin-verification-tokens", text: "NAV__VERIFICATION_TOKENS" },
+  { icon: "user", route: "admin-services", text: "NAV__SERVICES" },
+  { icon: "user", route: "admin-service-types", text: "NAV__SERVICE_TYPES" },
+  { icon: "receipt", route: "admin-invoices", text: "NAV__INVOICES" },
+  { icon: "money-check", route: "admin-payments", text: "NAV__PAYMENTS" },
+  { icon: "user", route: "admin-service-providers", text: "NAV__SERVICE_PROVIDERS" },
+  { icon: "network-wired", route: "admin-oauth-clients", text: "NAV__OAUTH_CLIENTS" }
+];
