29514ae1d2d3d141bd205921639217ba74f604e8
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 12:27:38 +0100

    feat: create sidebar link & dropdown components
    * Create `SidebarDropdown` component
    * Create `SidebarLink` component

---
+++ components/SidebarLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="nav-item">
+    <b-link active-class="active" exact :to="{ name: route }" class="nav-link">
+      <fa-icon v-if="icon" :icon="icon" class="nav-icon fa-fw" />
+      <p>{{ text }}</p>
+    </b-link>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      icon: {
+        type: [Array, String],
+        required: false,
+        default() {
+          return null;
+        }
+      },
+      isChild: Boolean,
+      route: {
+        type: String,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  };
+</script>
