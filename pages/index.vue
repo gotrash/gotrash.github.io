8f40b6928ccb2ed9f4d3942461b2962b438c5f9d
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 21:56:39 +0100

    feat: update home page
    * WIP Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>
-    <h1>{{ $t("PAGE_TITLE__HOME") }}</h1>
+  <div class="d-flex align-items-center justify-content-around h-100">
+    <b-card class="rounded-lg" body-bg-variant="dark" footer-bg-variant="dark" header-bg-variant="dark">
+      <div>
+        <h1>{{ $t("PAGE_TITLE__HOME") }}</h1>
+      </div>
+      <b-form>
+        <b-form-group label="Postcode">
+          <b-form-input id="postcode-input" class="custom-text-field" />
+        </b-form-group>
+      </b-form>
+    </b-card>
   </div>
 </template>
 
 <script>
   export default {
-    name: "IndexPage",
-    auth: false
+    name: "HomePage",
+    auth: false,
+    layout: "home",
+    computed: {
+      height() {
+        if (!process.client) return null;
+
+        return window.innerHeight + "px";
+      }
+    }
   };
 </script>
+
+<style lang="scss">
+  .custom-text-field {
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid $white;
+    border-radius: 0px;
+  }
+</style>
