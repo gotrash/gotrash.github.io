8353a55d3478b4e854d6e89824752877fd80ca6b
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 22:02:23 +0100

    feat: create users index page

---
+++ pages/users/users/index.vue
@@ -0,0 +1,20 @@
+<script>
+  import { PagedJpaTablePage } from "~/mixins";
+
+  export default {
+    name: "RolesIndex",
+    mixins: [PagedJpaTablePage],
+    apiUrl: "/users",
+    layout: "users",
+    rootRoute: "users-users",
+    pageTitleTranslation: "NAV__USERS",
+    showFilters: false,
+    data() {
+      // const $t = this.$t.bind(this);
+
+      return {
+        fields: []
+      };
+    }
+  };
+</script>
