8a4f23e027c89f971d7a6b0c4f50a39a44c62c71
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 21:57:28 +0100

    feat: create admin dashboard page
    Create admin dashboard page

---
+++ pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="12">
+        <h1>{{ $t("NAV__ADMIN_DASHBOARD") }}</h1>
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+      <b-col cols="12" sm="4" md="3">
+        <small-box icon="cog" heading="150" sub-heading="New Orders" />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    layout: "admin"
+  };
+</script>
