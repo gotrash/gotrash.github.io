9a3c2014ecc7b6e0af3920540f5c811531ae8490
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 12:29:41 +0100

    refactor: refactor `AdminSidebar` to use base component and config file
    Refactor `AdminSidebar` to use new components and config file

--- components/sidebars/AdminSidebar.vue
+++ components/sidebars/AdminSidebar.vue
@@ -1,31 +1,31 @@
 <template>
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <admin-lte-sidebar :menu="menu">
     <!-- Brand Logo -->
-    <b-link :to="{ name: 'index' }" class="brand-link">
-      <img
-        v-if="false"
-        src="dist/img/AdminLTELogo.png"
-        alt="AdminLTE Logo"
-        class="brand-image img-circle elevation-3"
-        style="opacity: 0.8"
-      />
-      <span class="brand-text font-weight-light"><b>Go</b>Trash</span>
-    </b-link>
+    <template #brand-link>
+      <b-link :to="{ name: 'index' }" class="brand-link">
+        <img
+          v-if="false"
+          src="dist/img/AdminLTELogo.png"
+          alt="GoTrash Logo"
+          class="brand-image img-circle elevation-3"
+          style="opacity: 0.8"
+        />
+        <span class="brand-text font-weight-light"><b>Go</b>Trash</span>
+      </b-link>
+    </template>
 
-    <!-- Sidebar -->
-    <div ref="sidebar" class="sidebar">
-      <!-- Sidebar user panel (optional) -->
+    <template v-if="false" #user-panel>
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img v-if="false" src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" />
+          <img :src="require('~/assets/images/user-placeholder.png')" class="img-circle elevation-2" alt="User Image" />
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ $auth.user.sub }}</a>
         </div>
       </div>
+    </template>
 
-      <!-- SidebarSearch Form -->
+    <template v-if="false" #search>
       <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
           <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
@@ -36,131 +36,24 @@
           </div>
         </div>
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <b-link
-              :to="{ name: 'admin-continents' }"
-              :class="['nav-link', { active: $route.name === 'admin-continents' }]"
-            >
-              <i class="nav-icon fas fa-th"></i>
-              <p>{{ $t("NAV__CONTINENTS") }}</p>
-            </b-link>
-          </li>
-          <li class="nav-item">
-            <b-link
-              :to="{ name: 'admin-countries' }"
-              :class="['nav-link', { active: $route.name === 'admin-countries' }]"
-            >
-              <i class="nav-icon fas fa-th"></i>
-              <p>{{ $t("NAV__COUNTRIES") }}</p>
-            </b-link>
-          </li>
-            <b-link :to="{ name: 'messages' }" :class="['nav-link', { active: $route.name === 'messages' }]">
-              <i class="nav-icon fas fa-th"></i>
-              <p>{{ $t("NAV__MESSAGES") }}</p>
-            </b-link>
-          </li>
-          <li class="nav-item">
-            <b-link :to="{ name: 'user' }" :class="['nav-link', { active: $route.name === 'user' }]">
-              <i class="nav-icon fas fa-th"></i>
-              <p>{{ $t("NAV__USER") }}</p>
-            </b-link>
-          </li>
-          <li :class="['nav-item', { active: $route.name === 'menu-open' }]">
-            <a href="#" :class="['nav-link', { active: $route.name === 'admin' }]">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Admin
-                <fa-icon icon="angle-left" class="right" />
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <b-link :to="{ name: 'admin' }" :class="['nav-link', { active: $route.name === 'admin' }]">
-                  <fa-icon :icon="['far', 'circle']" class="nav-icon" />
-                  <p>Admin</p>
-                </b-link>
-              </li>
-            </ul>
-          </li>
-          <li :class="['nav-item', { active: $route.name === 'menu-open' }]">
-            <a href="#" :class="['nav-link', { active: $route.name === 'admin-oauth-registered-clients' }]">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                {{ $t("NAV__OAUTH") }}
-                <fa-icon icon="angle-left" class="right" />
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <b-link
-                  :to="{ name: 'admin-oauth-registered-clients' }"
-                  :class="['nav-link', { active: $route.name === 'admin-oauth-registered-clients' }]"
-                >
-                  <fa-icon :icon="['far', 'circle']" class="nav-icon" />
-                  <p>{{ $t("NAV__REGISTERED_CLIENTS") }}</p>
-                </b-link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+    </template>
+  </admin-lte-sidebar>
 </template>
 
 <script>
+  import menu from "~/menus/AdminSidebar";
+
   export default {
     name: "AdminSidebar",
-    computed: {
-      sidebarHeight: {
-        get() {
-          return this.$store.getters("layout/getHeight")("sidebar");
-        },
-        set(height) {
-          this.$store.commit("layout/setHeight", { key: "sidebar", height });
-        }
-      },
-      sidebarWidth: {
-        get() {
-          return this.$store.getters("layout/getWidth")("sidebar");
-        },
-        set(width) {
-          this.$store.commit("layout/setWidth", { key: "sidebar", width });
-        }
-      }
+    components: {
+      AdminLteSidebar: () => import("~/components/sidebars/AdminLteSidebar"),
+      SidebarDropdown: () => import("~/components/SidebarDropdown"),
+      SidebarLink: () => import("~/components/SidebarLink")
     },
-    mounted() {
-      this.setHeight();
-      this.setWidth();
-
-      this.setupListeners();
-    },
-    unmounted() {
-      this.removeListeners();
-    },
-    methods: {
-      removeListeners() {
-        window.removeEventListener("resize", this.setHeight);
-        window.removeEventListener("resize", this.setWidth);
-      },
-      setHeight() {
-        this.sidebarHeight = this.$refs.sidebar.clientHeight;
-      },
-      setWidth() {
-        this.sidebarWidth = this.$refs.sidebar.clientWidth;
-      },
-      setupListeners() {
-        window.addEventListener("resize", this.setHeight);
-        window.addEventListener("resize", this.setWidth);
-      }
+    data() {
+      return {
+        menu
+      };
     }
   };
 </script>
