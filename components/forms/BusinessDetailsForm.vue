d6308bf37c05cf3c781c6691f2525e4eb2c4ca66
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 21:59:09 +0100

    feat: create business details form
    WIP Create business details form

---
+++ components/forms/BusinessDetailsForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-form>
+    <b-form-group :label="$t('BUSINESS.LABEL.BUSINESS_NAME')" label-for="business-name-input">
+      <b-form-input id="business-name-input" />
+    </b-form-group>
+  </b-form>
+</template>
+
+<script>
+  export default {};
+</script>
