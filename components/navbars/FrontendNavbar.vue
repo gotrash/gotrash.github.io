214f71c45a9c8b1d3b010fac8b980c4764d3b74f
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 19:57:21 +0100

    feat: create required navbars
    * Create `FrontendNavbar` component
    * Create `SettingsNavbar` component

---
+++ components/navbars/FrontendNavbar.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/no-v-html -->
+<template>
+  <b-navbar type="dark">
+    <b-navbar-nav>
+      <b-nav-item :to="{ name: 'index' }">{{ $t("NAV__HOME") }}</b-nav-item>
+    </b-navbar-nav>
+    <secondary-navbar-nav class="ml-auto" />
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    name: "SiteNavbar",
+    components: {
+      SecondaryNavbarNav: () => import("./navbar-navs/SecondaryNavbarNav.vue")
+    }
+  };
+</script>
+
+<style lang="scss">
+  #frontend-navbar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+</style>
