b0f06555819fb4a470ab3192e118f86861fd72c6
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-25 19:49:08 +0100

    fix: add `SmallBox` to global components plugin
    Add `SmallBox` to global components plugin

--- plugins/global-components.js
+++ plugins/global-components.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 
 import MessageSpinner from "~/global/components/MessageSpinner";
+import SmallBox from "~/global/components/SmallBox";
 
 Vue.component("MessageSpinner", MessageSpinner);
+Vue.component("SmallBox", SmallBox);
