d7a764ddef37a045bd570acaa5457d1915f87b11
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-26 20:39:50 +0100

    feat: create `PageUtils` mixin
    Create a `PageUtils` mixin

---
+++ mixins/PageUtils.vue
@@ -0,0 +1,11 @@
+<script>
+  export default {
+    methods: {
+      renderContainer(content = "No Content") {
+        const h = this.$createElement;
+
+        return h("b-container", { props: { fluid: true } }, [h("b-row", {}, [h("b-col", {}, content)])]);
+      }
+    }
+  };
+</script>
