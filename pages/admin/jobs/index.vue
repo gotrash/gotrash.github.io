7fd35acff878fe113a6f38a84f4eba98e112ca96
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-05-28 08:53:26 +0100

    feat: improve admin jobs index
    Improve admin jobs index page

--- pages/admin/jobs/index.vue
+++ pages/admin/jobs/index.vue
@@ -1,18 +1,38 @@
 <script>
+  import { format } from "date-fns";
   import { PagedJpaTablePage } from "~/mixins";
+  import { DateTimeFormats } from "~/constants";
 
   export default {
     name: "JobsIndex",
     mixins: [PagedJpaTablePage],
     apiUrl: "/jobs",
-    rootRoute: "admin-jobs",
+    rootRoute: "jobs-jobs",
     pageTitleTranslation: "NAV__JOBS",
     showFilters: false,
+    layout: "admin",
     data() {
-      // const $t = this.$t.bind(this);
+      const $t = this.$t.bind(this);
 
       return {
-        fields: []
+        fields: [
+          { key: "id", label: $t("JOBS.LABEL.ID") },
+          {
+            key: "jobType",
+            label: $t("JOBS.LABEL.JOB_TYPE"),
+            formatter: value => this.$t(`JOB_TYPES.TYPE.${value.machineName}`)
+          },
+          {
+            key: "createdAt",
+            label: $t("JOBS.LABEL.CREATED_AT"),
+            formatter: value => (value ? format(new Date(value), DateTimeFormats.DISPLAY_DATE_TIME) : null)
+          },
+          {
+            key: "updatedAt",
+            label: $t("JOBS.LABEL.UPDATED_AT"),
+            formatter: value => (value ? format(new Date(value), DateTimeFormats.DISPLAY_DATE_TIME) : null)
+          }
+        ]
       };
     }
   };
