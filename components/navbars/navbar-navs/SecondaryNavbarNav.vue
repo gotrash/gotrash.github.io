f39bba02509f8e1f7a6035f9d7da62f99ab4f656
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-06-09 11:24:26 +0100

    feat: update secondary navbar nav styling

--- components/navbars/navbar-navs/SecondaryNavbarNav.vue
+++ components/navbars/navbar-navs/SecondaryNavbarNav.vue
@@ -1,14 +1,21 @@
 <template>
-  <b-navbar-nav>
+  <b-navbar-nav class="font-weight-bolder">
     <fullscreen-navbar-item v-if="showFullscreenToggle" />
 
     <li v-if="showSidebarToggle" class="nav-item">
-      <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+      <a
+        class="nav-link text-success"
+        data-widget="control-sidebar"
+        data-controlsidebar-slide="true"
+        href="#"
+        role="button"
+      >
         <fa-icon icon="th-large" />
       </a>
     </li>
     <template v-if="!$auth.loggedIn">
       <b-nav-item
+        link-classes="text-success"
         @click="
           () => {
             $auth.loginWith('oidc');
@@ -16,25 +23,50 @@
         "
         >{{ $t("NAV__LOGIN") }}</b-nav-item
       >
-      <b-nav-item :to="{ name: 'auth-sign-up' }">{{ $t("NAV__SIGNUP") }}</b-nav-item>
+      <b-nav-item link-classes="text-success" :to="{ name: 'auth-sign-up' }">
+        {{ $t("NAV__SIGNUP") }}
+      </b-nav-item>
     </template>
     <template v-else>
-      <b-nav-item active-class="active" exact :label="$t('NAV__USER')" :to="{ name: 'admin-user' }">
-        <fa-icon icon="user" />
-      </b-nav-item>
-      <b-nav-item active-class="active" :label="$t('NAV__BUSINESS_PROFILE')" :to="{ name: 'business' }">
-        <fa-icon icon="building" />
-      </b-nav-item>
-      <b-nav-item active-class="active" exact :label="$t('NAV__SETTINGS')" :to="{ name: 'admin' }">
-        <fa-icon icon="cog" />
-      </b-nav-item>
-      <b-nav-item active-class="active" :label="$t('NAV__USERS_DASHBOARD')" :to="{ name: 'users' }">
-        <fa-icon icon="users" />
-      </b-nav-item>
-      <b-nav-item active-class="active" :label="$t('NAV__JOBS')" :to="{ name: 'jobs' }">
-        <fa-icon icon="tasks" />
+      <b-nav-item
+        link-classes="text-success"
+        active-class="active"
+        exact
+        :label="$t('NAV__USER')"
+        :to="{ name: 'admin-user' }"
+      >
+        <fa-icon icon="user" /><span class="d-none d-md-inline"> {{ $t("NAV__USER_PROFILE") }}</span>
       </b-nav-item>
       <b-nav-item
+        link-classes="text-success"
+        active-class="active"
+        :label="$t('NAV__BUSINESS_PROFILE')"
+        :to="{ name: 'business' }"
+      >
+        <fa-icon icon="building" /><span class="d-none d-md-inline"> {{ $t("NAV__BUSINESS") }}</span>
+      </b-nav-item>
+      <b-nav-item
+        link-classes="text-success"
+        active-class="active"
+        exact
+        :label="$t('NAV__SETTINGS')"
+        :to="{ name: 'admin' }"
+      >
+        <fa-icon icon="cog" /><span class="d-none d-md-inline"> {{ $t("NAV__SETTINGS") }}</span>
+      </b-nav-item>
+      <b-nav-item
+        link-classes="text-success"
+        active-class="active"
+        :label="$t('NAV__USERS_DASHBOARD')"
+        :to="{ name: 'users' }"
+      >
+        <fa-icon icon="users" /><span class="d-none d-md-inline"> {{ $t("NAV__SETTINGS") }}</span>
+      </b-nav-item>
+      <b-nav-item link-classes="text-success" active-class="active" :label="$t('NAV__JOBS')" :to="{ name: 'jobs' }">
+        <fa-icon icon="tasks" /><span class="d-none d-md-inline"> {{ $t("NAV__JOBS") }}</span>
+      </b-nav-item>
+      <b-nav-item
+        link-classes="text-success"
         :title="$t('NAV__LOGOUT')"
         @click="
           () => {
@@ -42,7 +74,7 @@
           }
         "
       >
-        <fa-icon icon="sign-out-alt" />
+        <fa-icon icon="sign-out-alt" /><span class="d-none d-md-inline"> {{ $t("NAV__LOGOUT") }}</span>
       </b-nav-item>
     </template>
     <b-nav-dropdown v-if="showLanguageSwitcher" id="nav--lang-dropdown" toggle-class="nav-link-custom" right>
