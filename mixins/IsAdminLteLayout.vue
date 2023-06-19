7de03ab78242b615aaa410db2c07c07f91e50185
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-06-19 19:04:41 +0100

    feat: create `IsAdminLteLayout` mixin

---
+++ mixins/IsAdminLteLayout.vue
@@ -0,0 +1,99 @@
+<script>
+  export default {
+    components: {
+      AdminFooter: () => import("~/components/footers/AdminFooter"),
+      AdminNavbar: () => import("~/components/navbars/AdminNavbar"),
+      AdminLteSidebar: () => import("~/components/sidebars/AdminLteSidebar"),
+      ControlSidebar: () => import("~/components/sidebars/ControlSidebar")
+    },
+    head() {
+      return {
+        bodyAttrs: {
+          class: "sidebar-mini layout-fixed"
+        },
+        script: [
+          {
+            src: "/js/jquery.min.js",
+            type: "text/javascript"
+          },
+          {
+            src: "/js/adminlte.min.js",
+            type: "text/javascript"
+          }
+        ]
+      };
+    },
+    computed: {
+      contentHeight() {
+        if (this.maxHeight === this.controlSidebarHeight) {
+          return this.maxHeight;
+        } else if (this.maxHeight === this.windowHeight) {
+          return this.maxHeight - this.headerHeight - this.footerHeight;
+        } else {
+          return this.maxHeight - this.headerHeight - this.footerHeight;
+        }
+      },
+      controlSidebarHeight() {
+        return this.$store.getters["layout/getHeight"]("controlSidebar");
+      },
+      footerHeight() {
+        return this.$store.getters["layout/getHeight"]("footer");
+      },
+      headerHeight() {
+        return this.$store.getters["layout/getHeight"]("header");
+      },
+      maxHeight() {
+        return this.$store.getters["layout/getMaxHeight"];
+      },
+      sidebarHeight() {
+        return this.$store.getters["layout/getHeight"]("sidebar");
+      },
+      windowHeight: {
+        get() {
+          return this.$store.getters["layout/getHeight"]("window");
+        },
+        set(height) {
+          this.$store.commit("layout/setHeight", { key: "window", height });
+        }
+      },
+      windowWidth: {
+        get() {
+          return this.$store.getters["layout/getWidth"]("window");
+        },
+        set(width) {
+          this.$store.commit("layout/setWidth", { key: "window", width });
+        }
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.init();
+
+        this.setupListeners();
+      });
+    },
+    unmounted() {
+      this.removeListeners();
+    },
+    methods: {
+      init() {
+        this.setHeight();
+        this.setWidth();
+      },
+      setHeight() {
+        this.windowHeight = window.innerHeight;
+      },
+      setWidth() {
+        this.windowWidth = window.innerWidth;
+      },
+      setupListeners() {
+        window.addEventListener("resize", this.setHeight);
+        window.addEventListener("resize", this.setWidth);
+      },
+      removeListeners() {
+        window.removeEventListener("resize", this.setHeight);
+        window.removeEventListener("resize", this.setWidth);
+      }
+    }
+  };
+</script>
