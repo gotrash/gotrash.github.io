af2fd6f103e1ece5df2742b4e6cd1a7f395261a4
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-06-29 11:23:30 +0100

    feat: create front page section components
    * Create `FrontpageAppLinksSection` component
    * Create `FrontpageSocialLinksSection` component
    * Create `FrontpageUsefulLinksSection` component

---
+++ components/sections/FrontpageSocialLinksSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <b-container fluid>
+      <b-row class="bg-gray-100">
+        <b-col>
+          <b-container>
+            <b-row>
+              <b-col cols="12" md="4" class="text-center py-5">
+                <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Find us on the app stores...</h4>
+                <div class="mx-auto" style="max-width: 420px">
+                  <p>Get notification on job updates by downloading our app from the usual places...</p>
+                </div>
+                <div>
+                  <img class="mx-1" style="max-width: 160px" :src="require('~/assets/images/google-play-badge.png')" />
+                  <img class="mx-1" style="max-width: 160px" :src="require('~/assets/images/google-play-badge.png')" />
+                </div>
+              </b-col>
+              <b-col cols="12" md="4" class="text-center py-5">
+                <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Find us on the app stores...</h4>
+                <div class="mx-auto" style="max-width: 420px">
+                  <p>Get notification on job updates by downloading our app from the usual places...</p>
+                </div>
+                <div>
+                  <img class="mx-1" style="max-width: 160px" :src="require('~/assets/images/google-play-badge.png')" />
+                  <img class="mx-1" style="max-width: 160px" :src="require('~/assets/images/google-play-badge.png')" />
+                </div>
+              </b-col>
+              <b-col cols="12" md="4" class="text-center py-5">
+                <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Find us on the app stores...</h4>
+                <div class="mx-auto" style="max-width: 420px">
+                  <p>Get notification on job updates by downloading our app from the usual places...</p>
+                </div>
+                <div>
+                  <img class="mx-1" style="max-width: 160px" :src="require('~/assets/images/google-play-badge.png')" />
+                  <img class="mx-1" style="max-width: 160px" :src="require('~/assets/images/google-play-badge.png')" />
+                </div>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
---
+++ components/sections/FrontpageUsefulLinksSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <b-container fluid>
+      <b-row class="py-5 bg-gray-300">
+        <b-col>
+          <b-container>
+            <h5 class="font-weight-bolder mb-3">Useful Links</h5>
+            <b-row class="mb-2">
+              <b-col cols="12" md="4">
+                <b-nav vertical>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'cookie-policy' }">Cookie Policy</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'modern-slavery-statement' }"
+                    >Modern Slavery Statement</b-nav-item
+                  >
+                  <b-nav-item link-classes="px-1" :to="{ name: 'privacy-policy' }">Privacy Policy</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'sustainability-policy' }"
+                    >Sustainability Policy</b-nav-item
+                  >
+                  <b-nav-item link-classes="px-1" :to="{ name: 'terms-and-conditions' }">Terms & Conditions</b-nav-item>
+                </b-nav>
+              </b-col>
+              <b-col cols="12" md="4">
+                <b-nav vertical>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'about-us' }">About Us</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'bug-bounty' }">Bug Bounty</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'contact-us' }">Contact Us</b-nav-item>
+                </b-nav>
+              </b-col>
+              <b-col cols="12" md="4">
+                <b-nav vertical>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'customer-signup' }">Customer Signup</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'provider-signup' }">Provider Signup</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'careers' }">Careers</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'business-signup' }">Business Signup</b-nav-item>
+                  <b-nav-item link-classes="px-1" :to="{ name: 'ethical-concerns' }">Ethical Concerns</b-nav-item>
+                </b-nav>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
