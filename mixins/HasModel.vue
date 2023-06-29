2ca5b69166b28e440f7c4a612436f1bad99e6502
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-06-29 11:29:39 +0100

    feat: improve `HasModel` mixin

--- mixins/HasModel.vue
+++ mixins/HasModel.vue
@@ -1,3 +1,21 @@
 <script>
-  export default {};
+  export default {
+    props: {
+      value: {
+        type: [Array, Number, Object, String],
+        required: false,
+        default: null
+      }
+    },
+    computed: {
+      model: {
+        get() {
+          return this.value;
+        },
+        set(model) {
+          this.$emit("input", model);
+        }
+      }
+    }
+  };
 </script>
