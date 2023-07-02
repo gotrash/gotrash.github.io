e6fbcbb2862b0805a439363a576dd296961e4693
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-07-02 22:25:27 +0100

    feat: create no providers page

---
+++ pages/no-providers.vue
@@ -0,0 +1,20 @@
+<template>
+  <fullscreen-section>
+    <b-alert class="px-6 py-4 text-center" variant="danger" show>
+      <h1 class="mb-4">No Providers</h1>
+      <p class="mb-4">Unfortunately no providers could be found that cover your postcode.</p>
+      <div>
+        <b-link :to="{ name: 'index' }">Go Home</b-link>
+      </div>
+    </b-alert>
+  </fullscreen-section>
+</template>
+
+<script>
+  import FullscreenSection from "~/components/sections/FullscreenSection";
+  export default {
+    components: {
+      FullscreenSection
+    }
+  };
+</script>
