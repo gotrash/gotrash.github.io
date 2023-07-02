8e7eb23bf40e65b41f7bf5707e74cd269bf076d9
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-07-02 22:22:30 +0100

    feat: create `FrontpageBlogSection` component

---
+++ components/sections/FrontpageBlogSection.vue
@@ -0,0 +1,122 @@
+<template>
+  <section>
+    <div></div>
+    <b-container fluid>
+      <b-row class="bg-gray-100">
+        <b-col>
+          <b-container>
+            <b-row>
+              <b-col cols="12">
+                <carousel
+                  class="py-6"
+                  per-page="3"
+                  :pagination-enabled="false"
+                  navigation-enabled
+                  :scroll-per-page="false"
+                >
+                  <slide class="text-center">
+                    <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Blog Article Title</h4>
+                    <div class="mx-auto" style="max-width: 420px">
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                    </div>
+                  </slide>
+                  <slide class="text-center">
+                    <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Blog Article Title</h4>
+                    <div class="mx-auto" style="max-width: 420px">
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                    </div>
+                  </slide>
+                  <slide class="text-center">
+                    <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Blog Article Title</h4>
+                    <div class="mx-auto" style="max-width: 420px">
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                    </div>
+                  </slide>
+                  <slide class="text-center">
+                    <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Blog Article Title</h4>
+                    <div class="mx-auto" style="max-width: 420px">
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                    </div>
+                  </slide>
+                  <slide class="text-center">
+                    <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Blog Article Title</h4>
+                    <div class="mx-auto" style="max-width: 420px">
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                    </div>
+                  </slide>
+                  <slide class="text-center">
+                    <h4 class="mx-auto mb-3 font-weight-bold" style="max-width: 420px">Blog Article Title</h4>
+                    <div class="mx-auto" style="max-width: 420px">
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                      <p>
+                        This would be some awesome blog article text, detailing some sort of regulation, standard or
+                        information that the general public might like to know.
+                      </p>
+                    </div>
+                  </slide>
+                </carousel>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      };
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true;
+      },
+      onSlideEnd(slide) {
+        this.sliding = false;
+      }
+    }
+  };
+</script>
