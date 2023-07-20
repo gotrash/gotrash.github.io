9efe310c6272a9136848586cf47a88929cb862be
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-07-20 19:39:10 +0100

    feat: new Nuxt plugins

---
+++ plugins/bootstrap-vue.js
@@ -0,0 +1,9 @@
+// import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+
+export default defineNuxtPlugin(app => {
+  // app.use(BootstrapVue);
+  // app.use(BootstrapVueIcons);
+  // console.log("Nuxt App: %o", Object.keys(app));
+  // console.log("Hooks: %o", app.hooks);
+  // console.log("Vue App: %o", app.vueApp);
+});
