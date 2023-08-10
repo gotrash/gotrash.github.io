301f2f680cdd08e4aa9ad2e854e4ef9dd88bcf7a
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-08-10 21:32:54 +0100

    fix: fixes in `BaseAuditableDto` DTO class

--- dto/BaseAuditableDto.js
+++ dto/BaseAuditableDto.js
@@ -1,6 +1,10 @@
 import BaseDto from "./BaseDto";
 
 export default class BaseAuditableDto extends BaseDto {
+  // ID
+  id = null;
+
+  // Audit Metadata
   createdAt = null;
   updatedAt = null;
 
@@ -8,6 +12,10 @@ export default class BaseAuditableDto extends BaseDto {
     super(data);
 
     if (data && typeof data === "object") {
+      // ID
+      if (Object.prototype.hasOwnProperty.call(data, "id") && !isNaN(data.id)) this.id = parseInt(parseFloat(data.id));
+
+      // Audit Metadata
       if (data?.createdAt && typeof data.createdAt === "string") this.createdAt = data.createdAt;
       if (data?.updatedAt && typeof data.updatedAt === "string") this.updatedAt = data.updatedAt;
     }
