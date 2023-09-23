3f2133424e5822cfc61a953882df982f14b713a7
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-09-23 20:25:58 +0100

    feat: WIP create payment method UI components

---
+++ components/PaymentMethodUI.vue
@@ -0,0 +1,35 @@
+<template>
+  <component class="mb-3" @delete="onDelete" :is="component" :payment-method="paymentMethod" />
+</template>
+
+<script setup>
+import PaymentMethodCardUI from "~/components/payment-methods/PaymentMethodCardUI.vue"
+import PaymentMethodUnknownUI from "~/components/payment-methods/PaymentMethodUnknownUI.vue"
+
+const emit = defineEmits(["delete"])
+
+const onDelete = paymentMethodId => {
+  console.log("Two: %o", paymentMethodId)
+  emit("delete", paymentMethodId);
+}
+
+const props = defineProps({
+  paymentMethod: {
+    type: Object,
+    validator(paymentMethod) {
+      const keys = Object.keys(paymentMethod);
+      return keys.includes("billingDetails")
+    }
+  }
+})
+
+const component = computed(() => {
+  switch (props.paymentMethod.type) {
+    case "card":
+      return PaymentMethodCardUI
+
+    default:
+      return PaymentMethodUnknownUI
+  }
+})
+</script>
