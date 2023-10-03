6b53181c5efd3dc98ca30ef7a1c2ccb1d3e81dbd
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-10-03 11:12:21 +0100

    feat: improve `BootstrapCreditCardForm`
    * Add Stripe test account data dropdown (debug)
    * Remove unused code

--- components/forms/BootstrapCreditCardForm.vue
+++ components/forms/BootstrapCreditCardForm.vue
@@ -1,5 +1,15 @@
 <template>
   <b-form>
+    <b-form-group v-if="showDebug || showTestData" label="Stripe Test Accounts"
+      :label-for="`stripe-test-data-picker-${_.uid}`" class="mb-3">
+      <b-form-select @update:modelValue="onInsertTestDetails" :model-value="null">
+        <b-form-select-option :value="null">Select an account to pre-fill the form below with test
+          data</b-form-select-option>
+        <b-form-select-option-group v-for="(status, statusKey) in StripeTestAccounts" :label="statusKey">
+          <b-form-select-option :value="card" v-for="card in status">{{ card.label }}</b-form-select-option>
+        </b-form-select-option-group>
+      </b-form-select>
+    </b-form-group>
     <b-form-group :label="$t('PAYMENT_METHODS.LABEL.CARD_NUMBER')" :label-for="`card-number-input-${_.uid}`" class="mb-3">
       <b-form-input v-number-only v-model="value.cardNumber" v-mask data-maska="####-####-####-####"
         :id="`card-number-input-${_.uid}`" />
@@ -18,24 +28,39 @@
           </b-col>
           <b-col cols="6" class="ps-1">
             <b-form-group :label="$t('PAYMENT_METHODS.LABEL.EXP_YEAR')" :label-for="`expiry-year-input-${_.uid}`">
-              <b-form-input v-model="value.expMonth" :id="`expiry-year-input-${_.uid}`" />
+              <b-form-input v-model="value.expYear" :id="`expiry-year-input-${_.uid}`" />
             </b-form-group>
           </b-col>
         </b-row>
       </b-col>
       <b-col cols="12" sm="4">
         <b-form-group label="CVV" :label-for="`cvv-input-${_.uid}`">
-          <b-form-input v-number-only :id="`cvv-input-${_.uid}`" v-model="value.cvv" />
+          <b-form-input :id="`cvv-input-${_.uid}`" v-model="value.cvc" />
         </b-form-group>
       </b-col>
     </b-row>
+    <div class="modal-footer border-0 p-0">
+      <b-button v-if="showReset" @click="$emit('reset')" variant="outline-danger">Reset</b-button>
+      <b-button v-if="showCancel" @click="$emit('cancel')" variant="danger" class="text-white">Cancel</b-button>
+      <b-button type="submit" variant="success" class="text-white">Save</b-button>
+    </div>
     <pre-code v-if="showDebug" v-model="value" />
   </b-form>
 </template>
 
 <script setup>
-const props = defineProps(["modelValue", "showDebug"]);
-const emit = defineEmits(["onSubmit", "update:modelValue"]);
+import StripeTestAccounts from "~/test/_data/stripe-accounts.json"
+const props = defineProps(["showCancel", "showReset", "modelValue", "showDebug"]);
+const emit = defineEmits(["cancel", "reset", "onSubmit", "update:modelValue"]);
+const route = useRoute();
+
+const showDebug = computed(() => {
+  return route.query.debug;
+})
+
+const showTestData = computed(() => {
+  return route.query.testData;
+})
 const value = computed({
   get() {
     return props.modelValue;
@@ -44,47 +69,16 @@ const value = computed({
     emit("update:modelValue", modelValue)
   }
 })
+const onInsertTestDetails = testAccountDetails => {
+  value.value.cardNumber = testAccountDetails.cardNumber
+  value.value.expMonth = testAccountDetails.expMonth;
+  value.value.expYear = testAccountDetails.expYear;
+  value.value.cvc = testAccountDetails.cvc;
+  value.value.cardHoldersName = testAccountDetails.label;
+}
+
 const onSubmit = (evt) => {
   emit("onSubmit", evt);
 }
 
-const vNumberOnly = {
-  mounted: (el) => {
-    console.log(el)
-  },
-  updated: (el) => {
-    function checkValue(event) {
-      console.log("Checking Value: %o", event)
-      event.target.value = event.target.value.replace(/[^0-9]/g, '')
-      if (event.charCode >= 48 && event.charCode <= 57) {
-        return true
-      }
-      event.preventDefault()
-    }
-    el.addEventListener('keypress', checkValue)
-  }
-}
-</script>
-
-<script>
-export default {
-  directives: {
-    'number-only': {
-      bind(el) {
-
-      }
-    },
-    'letter-only': {
-      bind(el) {
-        function checkValue(event) {
-          if (event.charCode >= 48 && event.charCode <= 57) {
-            event.preventDefault()
-          }
-          return true
-        }
-        el.addEventListener('keypress', checkValue)
-      }
-    }
-  }
-}
 </script>
