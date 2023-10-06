3dce608a819670336376b9b5a55bd5bd0f08ea29
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-10-06 09:56:37 +0100

    fix: fix spinner message in `PageStatusRow` component

--- global/components/PageStatusRow.vue
+++ global/components/PageStatusRow.vue
@@ -2,9 +2,7 @@
   <!-- If we have an error or we're loading data, show an alert to the user -->
   <b-row v-if="error || busy">
     <b-col cols="12">
-      <message-spinner v-if="busy">
-        {{ message }}
-      </message-spinner>
+      <message-spinner v-if="busy" :message="busyMessage" />
       <error-alert v-else-if="error" :error="error" />
     </b-col>
   </b-row>
