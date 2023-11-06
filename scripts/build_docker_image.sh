fb7de208d20e0f171d864cd473714b837e6d80d9
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-11-06 17:31:55 +0000

    fix: nDocker build

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -2,4 +2,4 @@
 
 export GIT_HASH=$(git rev-parse --verify --short HEAD)
 
-docker build -t 192.168.1.70:32000/gotrash/webapp:$GIT_HASH $PWD
+docker build --build-arg AUTH_ORIGIN=https://staging.gotrash.co.uk --build-arg NUXT_AUTH_ORIGIN=https://staging.gotrash.co.uk -t 192.168.1.70:32000/gotrash/webapp:$GIT_HASH $PWD
