acbc46daf659a04170477ed7b6abdc45e4371164
Alex 'Codetheorist' Scott <codetheorist@outlook.com>
2023-11-07 09:58:55 +0000

    fix: runtime config

--- config/runtimeConfig.js
+++ config/runtimeConfig.js
@@ -1,16 +1,17 @@
 export default {
-  authUrl: process.env.AUTH_URL,
-  apiUrl: process.env.API_URL,
-  baseUrl: process.env.BASE_URL,
+  authUrl: process.env.AUTH_URL || "http://localhost:9000",
+  apiUrl: process.env.API_URL || "http://localhost:8090",
+  baseUrl: process.env.BASE_URL || "http://localhost:3000",
   auth: {
-    origin: process.env.AUTH_ORIGIN
+    origin: process.env.AUTH_ORIGIN || "http://localhost:3000"
   },
   public: {
-    authUrl: process.env.AUTH_URL,
-    apiUrl: process.env.API_URL,
-    baseUrl: process.env.BASE_URL,
+    authSecret: process.env.AUTH_SECRET || "super-secret-localhost",
+    authUrl: process.env.AUTH_URL || "http://localhost:9000",
+    apiUrl: process.env.API_URL || "http://localhost:8090",
+    baseUrl: process.env.BASE_URL || "http://localhost:3000",
     auth: {
-      origin: process.env.AUTH_ORIGIN
+      origin: process.env.AUTH_ORIGIN || "http://localhost:3000"
     }
   }
 };
