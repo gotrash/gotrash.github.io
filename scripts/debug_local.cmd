4e5ae2787644291884cedd005b9ef23804a3810a
CodeTheorist <CodeTheorist@Outlook.Com>
2024-01-29 15:50:51 +0000

    fix: start-up scripts

--- scripts/debug_local.cmd
+++ scripts/debug_local.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set API_URL="http://localhost:8090"
-set AUTH_URL="http://localhost:9000"
+set API_URL="http://192.168.1.76:8090"
+set AUTH_URL="http://192.168.1.76:9000"
 
 npm run dev
--- scripts/debug_local.sh
+++ scripts/debug_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-API_URL="http://localhost:8090" \
-  AUTH_URL="http://localhost:9000" \
+API_URL="http://192.168.1.76:8090" \
+  AUTH_URL="http://192.168.1.76:9000" \
   npm run dev
