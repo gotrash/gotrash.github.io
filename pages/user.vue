c99a6f2be7112041ef85bfb8b129920ee298b6d6
CodeTheorist <CodeTheorist@Outlook.Com>
2024-02-07 22:00:54 +0000

    feat: create user site section wrapper page

---
+++ pages/user.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: "user",
+    scrollToTop: true
+  });
+
+  useHead({
+    titleTemplate: title => (title ? `${title} | GoTrash` : `GoTrash`)
+  });
+</script>
