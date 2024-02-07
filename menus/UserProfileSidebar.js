91ab1e3d410aae6f5a9d435e22b82691a659e451
CodeTheorist <CodeTheorist@Outlook.Com>
2024-02-07 13:19:41 +0000

    feat: create `UserProfileSidebar` menu definition file

---
+++ menus/UserProfileSidebar.js
@@ -0,0 +1,7 @@
+export default [
+  { text: "NAV__DASHBOARD", route: "user" },
+  { text: "NAV__FEEDBACK", route: "user-feedback" },
+  { text: "NAV__JOBS", route: "user-jobs" },
+  { text: "NAV__ADDRESSES", route: "user-addresses" },
+  { text: "NAV__PAYMENT_METHODS", route: "user-payment-methods" }
+];
