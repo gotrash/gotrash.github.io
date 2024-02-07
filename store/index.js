955c96bbddb804de81e005bc4aeb450423f393f1
CodeTheorist <CodeTheorist@Outlook.Com>
2024-02-07 13:18:23 +0000

    fix: rename root Pinia store to avoid conflicts

--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore("counter", {
+export const useRootStore = defineStore("root", {
   getters: {},
-  state: () => ({})
+  state: () => ({}),
+  actions: {}
 });
