18849a6c11fbd6eca1fb49633a4980a46260e1f7
CodeTheorist <CodeTheorist@Outlook.Com>
2024-04-06 18:51:13 +0100

    fix: layouts

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,61 +1,41 @@
 <template>
   <div id="wrapper">
-    <admin-navbar />
-
+    <b-navbar ref="header" class="main-header navbar-white" type="light">
+      <b-navbar-nav>
+        <b-nav-item>Home</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <admin-lte-sidebar :menu="menu" />
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Main content -->
-      <section class="content py-3" :style="{ 'min-height': `${contentHeight}px` }">
-        <!-- Content Header (Page header) -->
-        <div v-if="false" class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1 v-if="false" class="m-0">Dashboard</h1>
-              </div>
-              <!-- /.col -->
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item active">Dashboard v1</li>
-                </ol>
-              </div>
-              <!-- /.col -->
-            </div>
-            <!-- /.row -->
-          </div>
-          <!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-        <slot />
-      </section>
-      <!-- /.content -->
+    <div class="content-wrapper pb-3" style="min-height: calc(100vh - 7.125rem)">
+      <slot />
     </div>
-
     <admin-footer />
   </div>
 </template>
 
-<script>
-  import IsAdminLteLayout from "~/mixins/IsAdminLteLayout";
-  // import menu from "~/menus/SettingsSidebar";
+<script setup>
   import AdminFooter from "~/components/footers/AdminFooter";
+  import AdminLteSidebar from "~/components/sidebars/AdminLteSidebar";
+  import menu from "~/menus/SettingsSidebar";
 
-  export default {
-    name: "SettingsLayout",
-    extends: IsAdminLteLayout,
-    data() {
-      return {
-        menu: []
-      };
+  const { data, signOut } = useAuth();
+  const route = useRoute();
+
+  useHead({
+    bodyAttrs: {
+      class: "sidebar-mini layout-fixed"
     },
-    components: {
-      AdminFooter,
-      AdminNavbar: () => import("~/components/navbars/AdminNavbar"),
-      AdminLteSidebar: () => import("~/components/sidebars/AdminLteSidebar"),
-      ControlSidebar: () => import("~/components/sidebars/ControlSidebar")
+    title: route.meta.title
+  });
+
+  onMounted(() => {
+    console.log("Mounted");
+    console.log("Auth Data: %o", data);
+
+    if (data?.session?.user?.image) {
+      useApi("/users/files/" + data.session.user.image, { responseType: "blob" }).then(res => {
+        console.log(res);
+      });
     }
-  };
+  });
 </script>
