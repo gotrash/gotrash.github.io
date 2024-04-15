32f90e3cd08f111f1ca14db777abf3c2cd377dce
CodeTheorist <CodeTheorist@Outlook.Com>
2024-04-15 14:05:11 +0100

    chore: check Jenkins Github

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,6 @@
-export default ["~/plugins/directives"];
+export default [
+  "~/plugins/directives"
+  // "~/plugins/fontawesome",
+  // "~/plugins/google-maps/index.js"
+  // "~/plugins/maska.ts"
+];
